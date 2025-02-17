d0f4b472a561ef9a45b2a54d6e447697471eb3fa
Nikolai Nanev <niki4etooo@gmail.com>
2025-02-17 12:24:11 +0200

    changes on the formatting of options text

--- include/main_entry.h
+++ include/main_entry.h
@@ -5,16 +5,13 @@
 #include <cctype>
 #include <map>
 
+
 class MainEntry
 {
 private:
 	bool isInitialized = false;
 	char type;
 	std::string title = "Collaboration v1.0\n\n";
-	std::string optionList = "\nList of options\n"
-							 "---------------------------------------------------------------------------\n"
-							 "(1) Render Engine\t\t (2) Physics Engine\t\t (3) Sound Engine\t\t (4) Scripting\n"
-							 "---------------------------------------------------------------------------\n\n";
 	std::string questionEntry = "Which tool are you going to use? (select option [1-4] or q for quit): ";
 	std::map<char, std::string> optionSet;
 
@@ -23,6 +20,9 @@ public:
 	void showMessage(char type);
 	bool init();
 	void run();
+
+	std::string separator(size_t width);
+	std::string optionList();
 };
 
 #endif
--- src/main_entry.cpp
+++ src/main_entry.cpp
@@ -5,11 +5,46 @@
 #include "../include/sound_engine/sound_engine.h"
 #include "../include/scripting/scripting.h"
 
+#include <sys/ioctl.h> // utilities for the terminal
+#include <unistd.h>	   //STDOUT_FILENO
+
+#include <sstream>
+
 // Third Party headers
 
 // fmt
 #include <fmt/core.h>
 #include <fmt/color.h>
+
+struct winsize w; // terminal size ( width/height (rows/cols))
+
+std::string MainEntry::separator(size_t width)
+{
+	std::string separator = std::string(width, '-');
+	return separator;
+}
+
+std::string MainEntry::optionList()
+{
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+	std::stringstream ss;
+	std::string separator = this->separator(w.ws_col - 2);
+
+	std::string optionsText = "Render Engine { 1 | R }  Physics Engine { 2 | P }  Sound Engine { 3 | S }  Scripting Engine { 4 | C }  Quit { q | Q }";
+	// Centering the text for options 
+	size_t space = w.ws_col - optionsText.length();
+	space /= 2;
+	std::string options = std::string(space, ' ') + optionsText + std::string(space, ' ');
+
+	ss << separator << std::endl;
+	ss << options << std::endl;
+	ss << separator << std::endl;
+
+	std::string info = ss.str();
+
+	return info;
+}
 
 MainEntry::MainEntry()
 {
@@ -36,59 +71,65 @@ void MainEntry::run()
 {
 	if (this->init())
 	{
+
 		// Main Menu
 		fmt::print(fg(fmt::color::bisque), "{0}\n{1}\n{2}",
 				   this->title,
-				   this->optionList,
+				   this->optionList(),
 				   this->questionEntry);
 
 		std::cin >> this->type;
 
-		if ((type >= '1' && type <= '4') || tolower(type) == 'q')
+		switch (type)
 		{
-			switch (type)
-			{
-			case '1':
-			{
-				// Render Engine
-				this->showMessage(type);
-				RenderEngine *pRender = new RenderEngine();
-				pRender->run();
-			}
-			break;
-			case '2':
-			{
-				// Physics Engine
-				this->showMessage(type);
-				PhysicsEngine *pPhysics = new PhysicsEngine();
-				pPhysics->run();
-			}
-			break;
-			case '3':
-			{
-				// Sound Engine
-				this->showMessage(type);
-				SoundEngine *pSounds = new SoundEngine();
-				pSounds->run();
-			}
-			break;
-			case '4':
-			{
-				// Scripting
-				this->showMessage(type);
-				Scripting *pScripting = new Scripting();
-				pScripting->run();
-			}
-			break;
-			case 'q':
-			{
-				// Quit
-				this->showMessage(type);
-			}
-			}
+		case '1':
+		case 'R':
+		case 'r':
+		{
+			// Render Engine
+			this->showMessage(type);
+			RenderEngine *pRender = new RenderEngine();
+			pRender->run();
 		}
-		else
+		break;
+		case '2':
+		case 'P':
+		case 'p':
 		{
+			// Physics Engine
+			this->showMessage(type);
+			PhysicsEngine *pPhysics = new PhysicsEngine();
+			pPhysics->run();
+		}
+		break;
+		case '3':
+		case 'S':
+		case 's':
+		{
+			// Sound Engine
+			this->showMessage(type);
+			SoundEngine *pSounds = new SoundEngine();
+			pSounds->run();
+		}
+		break;
+		case '4':
+		case 'C':
+		case 'c':
+		{
+			// Scripting
+			this->showMessage(type);
+			Scripting *pScripting = new Scripting();
+			pScripting->run();
+		}
+		break;
+		case 'q':
+		case 'Q':
+		{
+			// Quit
+			this->showMessage(type);
+		}
+		break;
+		default:
 			fmt::print(fg(fmt::color::red), "Please, try another option.\n"); // Out of range option
 		}
 	}
